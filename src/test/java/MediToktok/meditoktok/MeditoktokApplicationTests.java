f9bcbf2c47b0f147680644cb2578ac0a36335652
zcxads <96506354+zcxads@users.noreply.github.com>
2023-02-06 16:28:55 +0900

    first commit

---
+++ src/test/java/MediToktok/meditoktok/MeditoktokApplicationTests.java
@@ -0,0 +1,13 @@
+package MediToktok.meditoktok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditoktokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
