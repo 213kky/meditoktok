4409387940d9c0f966c178e570d61fd8ed071575
SeungMin <ksm15910@naver.com>
2023-02-07 15:23:56 +0900

    first commit

---
+++ meditoktok/src/test/java/MediTokTok/meditoktok/MeditoktokApplicationTests.java
@@ -0,0 +1,13 @@
+package MediTokTok.meditoktok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditoktokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
