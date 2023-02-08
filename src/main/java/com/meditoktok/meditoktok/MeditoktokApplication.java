dbb4e8b69b2159857b9ebbe710a2999d539c1971
213kky <108410827+213kky@users.noreply.github.com>
2023-02-08 16:46:14 +0900

    setting

---
+++ src/main/java/com/meditoktok/meditoktok/MeditoktokApplication.java
@@ -0,0 +1,13 @@
+package com.meditoktok.meditoktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditoktokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeditoktokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meditoktok/meditoktok/MeditoktokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meditoktok.meditoktok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditoktokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
