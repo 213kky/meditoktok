750a96973c1410816992208a661481511d0f2a88
213kky <108410827+213kky@users.noreply.github.com>
2023-03-31 00:24:12 +0900

    뭐가 지워졌다 생겼다

---
+++ src/main/java/com/meditoktok/meditoktok/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.meditoktok.meditoktok.Repository;
+
+import com.meditoktok.meditoktok.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+
+}
