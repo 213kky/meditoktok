8ff09555f67bc772c0cb257198fd342c3375ba6c
jeongeonK <120458630+jeongeonK@users.noreply.github.com>
2023-04-10 20:03:08 +0900

    Dropdown 수정

--- src/main/frontend/src/component/component/Dropdown.js
+++ src/main/frontend/src/component/component/Dropdown.js
@@ -1,15 +1,34 @@
-import './Dropdown.css'
+import React, { useState } from 'react';
+import dummydata from './dummydata.json';
+import './Dropdown.css';
 
-export default function Dropdown() {
+function Dropdown() {
+  const [selectedOption, setSelectedOption] = useState('');
 
-return (
-<div className="Dropdown">
-<select id="browser" classname="doctor">
-  <option value="">의사명</option>
-  <option value="A">의사A</option>
-  <option value="B">의사B</option>
-  <option value="C">의사C</option>
-</select>
-</div>
+  function handleChange(event) {
+    setSelectedOption(event.target.value);
+  }
+
+  return (
+    <div>
+      <select className='Dropdown' value={selectedOption} onChange={handleChange}>
+        <option value="">--Select--</option>
+        {dummydata.map((name) => (
+          <option key={name.id} value={name.id}>
+            {name.name}
+          </option>
+        ))}
+      </select>
+      {/* {selectedOption && (
+        <div>
+          <h3>User Details</h3>
+          <p>Name: {dummyData[selectedOption - 1].name}</p>
+          <p>Age: {dummyData[selectedOption - 1].age}</p>
+          <p>Gender: {dummyData[selectedOption - 1].gender}</p>
+        </div>
+      )} */}
+    </div>
   );
 }
+
+export default Dropdown;
