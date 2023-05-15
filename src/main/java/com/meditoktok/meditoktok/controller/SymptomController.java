bd3cb6c3ac59a34aa0e3d3c088dd80ab4c284469
SeungMin <ksm15910@naver.com>
2023-05-15 19:59:58 +0900

    카카오 로그인 화면 전환 및 증상 검색 기능 추가

---
+++ src/main/java/com/meditoktok/meditoktok/controller/SymptomController.java
@@ -0,0 +1,24 @@
+package com.meditoktok.meditoktok.controller;
+
+import com.meditoktok.meditoktok.domain.Symptom;
+import com.meditoktok.meditoktok.service.SymptomService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/symptoms")
+public class SymptomController {
+    private final SymptomService symptomService;
+
+    public SymptomController(SymptomService symptomService) {
+        this.symptomService = symptomService;
+    }
+
+    @GetMapping
+    public List<Symptom> getAllSymptoms() {
+        return symptomService.getAllSymptoms();
+    }
+}
