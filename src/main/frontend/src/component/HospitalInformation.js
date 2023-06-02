46d15ddd479668c28eec8f9bd79136c62298b20a
JeonJW24 <tyui153@naver.com>
2023-06-02 12:58:07 +0900

    달력날짜 수정 및 로그인 수정

--- src/main/frontend/src/component/HospitalInformation.js
+++ src/main/frontend/src/component/HospitalInformation.js
@@ -73,7 +73,7 @@ export default function HospitalInformation() {
                         },
                     });
                     setJoinData(response.data);
-
+                    setIsJoin(true);
                     console.log(response);
                 } catch (error) {
                     console.error('Error:', error);
