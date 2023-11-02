41a4710617b7faa64ab908b826dc1fcd2f5e8c0c
213kky <108410827+213kky@users.noreply.github.com>
2023-11-02 18:11:20 +0900

    글씨체 달력

--- src/main/frontend/src/component/component/Calendar.js
+++ src/main/frontend/src/component/component/Calendar.js
@@ -1,109 +1,121 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import 'moment/locale/ko';
-import './Calendar.css';
 
 moment.locale('ko');
 
-function Calendar({clickedDate, setClickedDate, setClickedTime}) {
-    const [selectedDate, setSelectedDate] = useState(moment());
-
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-        setSelectedDate((prevDate) => prevDate.clone().set(name, value).startOf('month'));
-    };
-
-    const handlePrevMonth = () => {
-        setSelectedDate((prevDate) => prevDate.clone().subtract(1, 'month').startOf('month'));
-    };
-
-    const handleNextMonth = () => {
-        setSelectedDate((prevDate) => prevDate.clone().add(1, 'month').startOf('month'));
-    };
-
-
-    const handleDateClick = (clickedDate) => {
+function Calendar({ clickedDate, setClickedDate, setClickedTime }) {
+  const [selectedDate, setSelectedDate] = useState(moment());
+ const handleDateClick = (clickedDate) => {
         setClickedDate(clickedDate);
-        if(typeof setClickedTime === "function" ){
+        if (typeof setClickedTime === "function") {
             setClickedTime(null);
         }
         console.log(clickedDate.format('YYYY-MM-DD')); // 선택된 날짜 출력
     };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setSelectedDate((prevDate) => prevDate.clone().set(name, value));
+  };
 
-    const weekdaysShort = moment.weekdaysShort(true);
-    const months = moment.months();
+  const handlePrevMonth = () => {
+    setSelectedDate((prevDate) => prevDate.clone().subtract(1, 'month'));
+  };
 
-    return (
-        <div className="calendar">
-            <div className="dropdowns">
-                <select name="year" value={selectedDate.format('YYYY')} onChange={handleChange}>
-                    {Array.from({length: 100}, (_, i) => moment().year() - i).map((year) => (
-                        <option key={year} value={year}>
-                            {year}
-                        </option>
-                    ))}
-                </select>
-                <select name="month" value={selectedDate.format('MM')} onChange={handleChange}>
-                    {months.map((month, i) => (
-                        <option key={month} value={i + 1}>
-                            {month}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="calendar-body">
-                <div className="calendar-header">
-                    <button onClick={handlePrevMonth}>{'<'}</button>
-                    <h2>{selectedDate.format('MMMM YYYY')}</h2>
-                    <button onClick={handleNextMonth}>{'>'}</button>
-                </div>
-                <table>
-                    <thead>
-                    <tr>
-                        {weekdaysShort.map((day) => (
-                            <th key={day}>{day}</th>
-                        ))}
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {Array.from({length: selectedDate.daysInMonth()}, (_, i) =>
-                        selectedDate.clone().date(i + 1)
-                    ).reduce(
-                        (rows, currentDate) => {
-                            if (rows[rows.length - 1].length === 7) {
-                                rows.push([currentDate]);
-                            } else {
-                                rows[rows.length - 1].push(currentDate);
-                            }
-                            return rows;
-                        },
-                        [[]]
-                    ).map((week, i) => (
-                        <tr key={i}>
-                            {week.map((day) => (
-                                <td
-                                    key={day.format('YYYY-MM-DD')}
-                                    className={
-                                        day.isSame(selectedDate, 'day')
-                                            ? day.isSame(clickedDate, 'day')
-                                                ? 'selected clicked'
-                                                : 'selected'
-                                            : day.isSame(clickedDate, 'day')
-                                                ? 'clicked'
-                                                : ''
-                                    }
-                                    onClick={() => handleDateClick(day)}
-                                >
-                                    {day.format('D')}
-                                </td>
-                            ))}
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
+  const handleNextMonth = () => {
+    setSelectedDate((prevDate) => prevDate.clone().add(1, 'month'));
+  };
+
+  const months = moment.months();
+
+  const firstDayOfMonth = moment(selectedDate).date(1);
+  const firstDayOfWeek = firstDayOfMonth.day();
+  const daysInMonth = selectedDate.daysInMonth();
+
+  const calendarRows = [];
+  let currentRow = [];
+
+  for (let i = 0; i < firstDayOfWeek; i++) {
+    currentRow.push(null);
+  }
+
+  for (let i = 1; i <= daysInMonth; i++) {
+    const currentDate = firstDayOfMonth.clone().date(i);
+    currentRow.push(currentDate);
+
+    if (currentRow.length === 7) {
+      calendarRows.push(currentRow);
+      currentRow = [];
+    }
+  }
+
+  if (currentRow.length > 0) {
+    calendarRows.push(currentRow);
+  }
+
+  const weekdaysShort = moment.weekdaysShort();
+
+  return (
+    <div className="calendar">
+      <div className="dropdowns">
+        <select name="year" value={selectedDate.format('YYYY')} onChange={handleChange}>
+          {Array.from({ length: 100 }, (_, i) => moment().year() - i).map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+        <select name="month" value={selectedDate.format('MM')} onChange={handleChange}>
+          {months.map((month, i) => (
+            <option key={month} value={i + 1}>
+              {month}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="calendar-body">
+        <div className="calendar-header">
+          <button onClick={handlePrevMonth}>{'<'}</button>
+          <h2>{selectedDate.format('MMMM YYYY')}</h2>
+          <button onClick={handleNextMonth}>{'>'}</button>
         </div>
-    );
+        <table>
+          <thead>
+            <tr>
+              {weekdaysShort.map((day) => (
+                <th key={day}>
+                  {day}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {calendarRows.map((week, i) => (
+              <tr key={i}>
+                {week.map((day, index) => (
+                  <td
+                    key={day ? day.format('YYYY-MM-DD') : index}
+                    className={
+                      day && day.isSame(selectedDate, 'day')
+                        ? day.isSame(clickedDate, 'day')
+                          ? 'selected clicked'
+                          : 'selected'
+                        : day && day.isSame(clickedDate, 'day')
+                        ? 'clicked'
+                        : ''
+                    }
+                    onClick={() => day && handleDateClick(day)}
+                  >
+                    {day && day.format('D')}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Calendar;
